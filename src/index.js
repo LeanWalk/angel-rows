99721804a97daec1d90425cd3a8018769bcadfc4
LeanWalk <mafrica53@yahoo.com>
2023-02-13 09:54:13 +0100

    migrating to Redux library library

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { Elements } from '@stripe/react-stripe-js';
 
 import App from './App';
-import { UserProvider } from './contexts/user.context';
-import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
+import { store } from './store/store'
+import { stripePromise } from './utils/stripe/stripe.utils';
+
 import './index.scss';
 
 
-
-const rootElement = document.getElementById('root');
-
-render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
+  <Provider store={store}>
   <BrowserRouter>
-  <UserProvider>
-  <CategoriesProvider>
   <CartProvider>
+  <Elements stripe={stripePromise}>
   <App />
+  </Elements>
   </CartProvider>
-  </CategoriesProvider>
-  </UserProvider>
   </BrowserRouter>  
+  </Provider>
   </React.StrictMode>,
-  rootElement
 );
